3dc5f453368d3b6b23b03a3d78d3a2d22386edec
Apriani magfira <57305690+13020180058@users.noreply.github.com>
2020-03-16 23:12:09 +0800

    Add files via upload

---
+++ fira/fira.java
@@ -0,0 +1,19 @@
+/*NamaFile : apriani
+/*Hari/tgl/waktu: senin,16 mar 2020 21.40 
+*/
+
+
+public class fira{
+	public static void main(String[] args){
+	String nim ="13020180058";
+	String nama ="Apriani Magfira";
+	String jurusan ="Teknik Informatika";
+	String fakultas ="ilmu komputer";
+	System.out.println("nim saya: "+ nim);
+	System.out.println("nama saya: "+ nama);
+	System.out.println("jurusan: "+ jurusan);
+	System.out.println("fakultas: "+ fakultas);
+
+}
+}
+	
